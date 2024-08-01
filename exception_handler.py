05b2b289158867ffa83a16d7194e170eee049690
Thomas John <106020285+devmanac@users.noreply.github.com>
2024-08-01 19:04:58 +0200

    Exception Handling

---
+++ exception_handler.py
@@ -0,0 +1,85 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "f66e6c56",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Wrong key! Could not get last name!\n",
+      "Completed\n",
+      "The actor's first name is John\n",
+      "Exception in Getting Value\n",
+      "All exceptions caught! Good job!\n"
+     ]
+    }
+   ],
+   "source": [
+    "# Setup\n",
+    "actor = {\"name\": \"John\", \"rank\": \"Lieutenant\"}\n",
+    "traits = ['comedian', 'villain', 'hero']\n",
+    "\n",
+    "# Function to modify!!!\n",
+    "def get_last_name(): \n",
+    "    return actor[\"last_name\"]\n",
+    "    \n",
+    "def get_name(): \n",
+    "    return actor[\"name\"]    \n",
+    "    \n",
+    "def get_value(index):\n",
+    "    return traits[index]    \n",
+    "    \n",
+    "\n",
+    "# Test code\n",
+    "try:\n",
+    "    get_last_name()\n",
+    "except KeyError:\n",
+    "    print('Wrong key! Could not get last name!')\n",
+    "finally:\n",
+    "    print('Completed')\n",
+    "\n",
+    "print(\"The actor's first name is %s\" % get_name())\n",
+    "\n",
+    "try:\n",
+    "    get_value(4)\n",
+    "except:\n",
+    "    print('Exception in Getting Value')\n",
+    "    \n",
+    "print(\"All exceptions caught! Good job!\")    \n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "73a10289",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
